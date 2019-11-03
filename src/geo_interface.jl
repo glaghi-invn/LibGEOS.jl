14bf8b6365abb95e0f4b20117fc30f375a7136b7
Martijn Visser <mgvisser@gmail.com>
2019-11-03 14:45:41 +0100

    partial interface

--- src/geo_interface.jl
+++ src/geo_interface.jl
@@ -64,3 +64,98 @@ GeoInterphase.geomtype(g::MultiPolygon) = GeoInterphase.MultiPolygon()
 GeoInterphase.geomtype(g::GeometryCollection) = GeoInterphase.GeometryCollection()
 GeoInterphase.geomtype(g::LinearRing) = GeoInterphase.LineString()
 # TODO handle PreparedGeometry
+
+
+function GeoInterphase.ncoord(g::Geometry)
+    cs = getCoordSeq(g.ptr)
+    getDimensions(cs)
+end
+
+function GeoInterphase.getcoord(g::Point, i::Int)
+    cs = getCoordSeq(g.ptr)
+    if i == 1
+        getX(cs, 1)
+    elseif i == 2
+        geYX(cs, 1)
+    elseif i == 3
+        getZ(cs, 1)
+    else
+        throw(ArgumentError("GEOS only supports 2 and 3 dimensional points"))
+    end
+end
+
+function GeoInterphase.npoint(g::LineString)
+    numPoints(g)
+end
+
+## came up to here
+
+function GeoInterphase.getpoint(g::LineString, i::Int)
+    cs = getCoordSeq(g.ptr)
+    Point(g[i])
+end
+
+# TODO what to return for length 0 and 1?
+# TODO should this be an approximate equals for floating point?
+function GeoInterphase.isclosed(g::LineString, i::Int)
+    cs = getCoordSeq(g.ptr)
+    first(g) == last(g)
+end
+
+# TODO this should return a "LineString" according to GeoInterphase, but this cannot directly
+# be identified as such, is that a problem?
+
+function GeoInterphase.getexterior(g::Polygon)
+    cs = getCoordSeq(g.ptr)
+    LineString(first(g))
+end
+
+function GeoInterphase.nhole(g::Polygon)
+    cs = getCoordSeq(g.ptr)
+    length(g) - 1
+end
+
+function GeoInterphase.gethole(g::Polygon, i::Int)
+    cs = getCoordSeq(g.ptr)
+    LineString(g[i + 1])
+end
+
+function GeoInterphase.npoint(g::MultiPoint)
+    cs = getCoordSeq(g.ptr)
+    length(g)
+end
+
+function GeoInterphase.getpoint(g::MultiPoint, i::Int)
+    cs = getCoordSeq(g.ptr)
+    Point(g[i])
+end
+
+function GeoInterphase.nlinestring(g::MultiLineString)
+    cs = getCoordSeq(g.ptr)
+    length(g)
+end
+
+function GeoInterphase.getlinestring(g::MultiLineString, i::Int)
+    cs = getCoordSeq(g.ptr)
+    LineString(g[i])
+end
+
+function GeoInterphase.npolygon(g::MultiPolygon)
+    cs = getCoordSeq(g.ptr)
+    length(g)
+end
+
+function GeoInterphase.getpolygon(g::MultiPolygon, i::Int)
+    cs = getCoordSeq(g.ptr)
+    LineString(g[i])
+end
+
+function GeoInterphase.ngeom(g::GeometryCollection)
+    cs = getCoordSeq(g.ptr)
+    length(g)
+end
+
+function GeoInterphase.getgeom(g::GeometryCollection, i::Int)
+    cs = getCoordSeq(g.ptr)
+    geometry(g[i])
+end
